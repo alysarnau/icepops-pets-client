c12cee349f7ffb542561ad9ef1e1273dcfa7d236
Alys <alice.g.cooper@gmail.com>
2022-08-01 15:24:07 -0400

    Set up toy routes

---
+++ src/api/toys.js
@@ -0,0 +1,39 @@
+import apiUrl from "../apiConfig";
+import axios from "axios";
+
+// CREATE
+export const createToy = (user, petId, newToy) => {
+    console.log('the user in createToy', user)
+    console.log('the newToy in createToy', newToy)
+	return axios({
+		url: `${apiUrl}/toys/${petId}`,
+		method: 'POST',
+        // deleted Authorization header bc it's not needed
+		data: { toy: newToy } ,
+	})
+}
+
+// UPDATE
+export const updateToy = (user, petId, updatedToy) => {
+    console.log('the user in updateToy', user)
+    console.log('the updated toy in updateToy', updatedToy)
+	return axios({
+		url: `${apiUrl}/toys/${petId}/${updatedToy.id}`,
+		method: 'PATCH',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		},
+		data: { toy: updatedToy } ,
+	})
+}
+
+// DELETE
+export const deleteToy = (user, petId, toyId) => {
+	return axios({
+		url: `${apiUrl}/toys/${petId}/${toyId}`,
+		method: 'DELETE',
+		headers: {
+			Authorization: `Token token=${user.token}`,
+		}
+	})
+}
